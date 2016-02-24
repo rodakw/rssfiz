411e963fd76729ad9d050e969464e529898fd432
wieslaw.rodak <wieslaw.rodak@EWADL00F6SJ0P1.ams.com>
2016-02-24 12:38:17 +0100

    walidacja

--- home/rssfiz.show/src/main/java/pl/wr/rss/rssfiz/show/ExampleControler.java
+++ home/rssfiz.show/src/main/java/pl/wr/rss/rssfiz/show/ExampleControler.java
@@ -1,16 +1,26 @@
 package pl.wr.rss.rssfiz.show;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import pl.wr.rss.rssfiz.show.model.ExampleForm;
+import pl.wr.rss.rssfiz.show.validator.ExampleValidator;
 
 @Controller
 @RequestMapping("/exampleForm")
 public class ExampleControler {
+
+    ExampleValidator exampleValidator;
+
+    @Autowired
+    public ExampleControler(ExampleValidator exampleValidator) {
+        this.exampleValidator = exampleValidator;
+    }
 
     @RequestMapping(method = RequestMethod.GET)
     public String viewForm(Model model) {
@@ -22,9 +32,13 @@ public class ExampleControler {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public String calculateForm(@ModelAttribute("myData") ExampleForm myData, Model model) {
+    public String calculateForm(@ModelAttribute("myData") ExampleForm myData, BindingResult result, Model model) {
 
-        model.addAttribute("result", myData.getParam1() + myData.getParam2() + myData.getParam3() + myData.getParam4());
+        exampleValidator.validate(myData, result);
+
+        if (!result.hasErrors()) {
+            model.addAttribute("result", myData.getParam1() + myData.getParam2() + myData.getParam3() + myData.getParam4());
+        }
 
         return "exampleForm";
     }
--- home/rssfiz.show/src/main/webapp/WEB-INF/pages/exampleForm.jsp
+++ home/rssfiz.show/src/main/webapp/WEB-INF/pages/exampleForm.jsp
@@ -1,21 +1,42 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%> 
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <html>
+
+<head>
+	<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+	<style>
+	.error {
+		color: #ff0000;
+	}
+	.errorblock{
+		color: #000;
+		background-color: #ffEEEE;
+		border: 3px solid #ff0000;
+		padding:8px;
+		margin:16px;
+	}
+
+	</style>
+</head>
 
 <body>
     <h2>Spring's form tags example</h2>
 
     <form:form method="POST" commandName="myData">
 
+	<form:errors path="*" cssClass="errorblock" element="div"/>
 
         <table>
             <tr>
                 <td>param1 :</td>
                 <td><form:input path="param1" /></td>
+                <td><form:errors path="param1" cssClass="error" /></td>
             </tr>
             <tr>
                 <td>param2 :</td>
                 <td><form:input path="param2" /></td>
+                <td><form:errors path="param2" cssClass="error" /></td>
             </tr>
 
             <tr>
