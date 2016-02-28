7ae0f4c7fd85c0958eabfd076735ffd692a86976
wieslaw.rodak <wieslaw.rodak@EWADL00F049XY1.ams.com>
2016-02-28 09:15:35 +0100

    link

--- math/src/main/java/pl/wr/rss/rssfiz/show/math/ComplexCalculatorControler.java
+++ math/src/main/java/pl/wr/rss/rssfiz/show/math/ComplexCalculatorControler.java
@@ -1,5 +1,10 @@
 package pl.wr.rss.rssfiz.show.math;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/complexCalculator")
 public class ComplexCalculatorControler {
 
 	// test
