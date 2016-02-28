3927312fb370716eb59cfc764c589e5a6cfdde70
Łukasz <ukasz@192.168.1.14>
2016-02-28 18:12:33 +0100

    Complex

--- math/src/main/java/pl/wr/rss/rssfiz/show/math/ComplexCalculatorControler.java
+++ math/src/main/java/pl/wr/rss/rssfiz/show/math/ComplexCalculatorControler.java
@@ -1,13 +1,24 @@
 package pl.wr.rss.rssfiz.show.math;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import pl.wr.rss.rssfiz.show.math.model.ComplexNumber;
 
 @Controller
 @RequestMapping("/complexCalculator")
 public class ComplexCalculatorControler {
 
-	// test
-	// test2a
-	//nowe
+	@RequestMapping(method = RequestMethod.GET)
+    public String viewForm(Model model) {
+
+        ComplexNumber complexNumber = new ComplexNumber();
+        complexNumber.setOperation("+");
+
+        model.addAttribute("myData", complexNumber);
+
+        return "fractionCalculator";
+    }
 }
---
+++ math/src/main/java/pl/wr/rss/rssfiz/show/math/model/ComplexNumber.java
@@ -0,0 +1,54 @@
+package pl.wr.rss.rssfiz.show.math.model;
+
+/**
+ * @author Łukasz
+ *
+ */
+public class ComplexNumber {
+
+	Double real1;
+	Double real2;
+	Double imaginary1;
+	Double imaginary2;
+	String operation;
+
+	public Double getReal1() {
+		return real1;
+	}
+
+	public void setReal1(Double real1) {
+		this.real1 = real1;
+	}
+
+	public Double getReal2() {
+		return real2;
+	}
+
+	public void setReal2(Double real2) {
+		this.real2 = real2;
+	}
+
+	public Double getImaginary1() {
+		return imaginary1;
+	}
+
+	public void setImaginary1(Double imaginary1) {
+		this.imaginary1 = imaginary1;
+	}
+
+	public Double getImaginary2() {
+		return imaginary2;
+	}
+
+	public void setImaginary2(Double imaginary2) {
+		this.imaginary2 = imaginary2;
+	}
+
+	public String getOperation() {
+		return operation;
+	}
+
+	public void setOperation(String operation) {
+		this.operation = operation;
+	}
+}
